9e2e5d28edcc34cfe15dc3e9e72ef71219fe280d
nitvit610 <nitvit610@gmail.com>
2015-11-20 14:24:34 +0000

    *

--- Classes.py
+++ Classes.py
@@ -1,19 +1,10 @@
-class Shape:
-    def __init__(self, x, y):
+class Squarea:
+    def __init__(self, x):
         self.x = x
-        self.y = y
-    description = "This shape has not been described yet"
-    author = "Nobody has claimed to make this shape yet"
     def area(self):
-        return self.x * self.y
-    def perimeter(self):
-        return 2 * self.x + 2 * self.y
-    def describe(self, text):
-        self.description = text
-    def authorName(self, text):
-        self.author = text
-    def scaleSize(self, scale):
-        self.x = self.x * scale
-    self.y = self.y * scale
+        area_total = self.x * self.x
+        print(area_total)
 
-rectangle = Shape(45, 100)
+square = Squarea(50)
+
+square.area()
